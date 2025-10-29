fc4069cbbfee9ac8051043b50d7aac0358f1bea3
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-10-29 09:10:59 +0100

    wip added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world')
+zmienna_1: str  = 'Natalia'
+zmienna_2: str  = 'Kasia'
+zmienna_3: str  = 'Michasia'
+zmienna_4: str  = 'Ewa'
+
+
+uzytkownicy:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f'Hello {uzytkownik}')
