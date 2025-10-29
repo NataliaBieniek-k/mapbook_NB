3fded6d40311fde75f668182d3de9b249b5fe41b
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-10-29 11:01:12 +0100

    wip: added function to added new user

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 users: list = [
-    {'name': 'Kasia', 'location': 'Warszawa', 'posts': 3},
-    {'name': 'Asia', 'location': 'Krakow', 'posts': 5},
-    {'name': 'Basia', 'location': 'Wroclaw', 'posts': 7},
-
+    {"name": "Kasia", "location": "Warszawa", "posts": 3},
+    {"name": "Basia", "location": "Kraków", "posts": 5},
+    {"name": "Asia", "location": "Wrocław", "posts": 7},
 ]
 
 
 def user_info(users_data: list) -> None:
     for user in users_data:
-        print(f'Twoja znajoma {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów ')
+        print(f"Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów ")
+
+
+def add_user(users_data) -> None:
+    name: str = input("Podaj imie nowego znajomego: ")
+    location: str = input("Podaj nazwę miejscowosci: ")
+    posts: int = int(input("Podaj liczbę postów: "))
+    users_data.append({"name": name, "location": location, "posts": posts})
 
 
 while True:
-    tmp_choice: int = int(input('Wybierz opcje menu'))
+    tmp_choice: int = int(input("Wybierz opcję menu"))
     if tmp_choice == 0:
         break
     if tmp_choice == 1:
-        print('Wybrano funkcję wyświetlania aktywności znajomych')
+        print("wybrano funkcje wyświetlania aktywności znajomych")
         user_info(users)
     if tmp_choice == 2:
-        print("Wybrano funkcję dodawania znajomego")
+        print("wybrano funkcje dodawania znajomego")
+        add_user(users)
     if tmp_choice == 3:
-        print('Wybrano funkcję usuwania znajomego')
+        print("wybrano funkcje usuwania znajomych")
     if tmp_choice == 4:
-        print('Wybrano funkcję aktualizacji danych znajomego')
-
-# user_info petla z 13 i 14 linijki
+        print("wybrano funkcje aktualizowania znajomych")
